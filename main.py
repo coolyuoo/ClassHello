773bbdd9ee46f5dedf58e370f0bcd6692e2abd44
ehmax <vsn80395@gmail.com>
2025-11-19 20:33:02 +0800

    666

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 
 @app.get("/")
-def read_root(name: str = "PyCharm"):
+def read_root(name: str = "PyCharm3.2"):
     """Root endpoint. Optional query param `name` (default: PyCharm)."""
     return {"message": f"Hi, {name}"}
 
