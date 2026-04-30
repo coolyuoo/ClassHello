d52eb153f5cb8ea979d969a7f9cbdb367e17ad5d
蛋頭 <vsn80395@gmail.com>
2026-04-30 21:39:23 +0800

    456

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 
 @app.get("/")
-def read_root(name: str = "666333"):
+def read_root(name: str = "20260430158962"):
     """Root endpoint. Optional query param `name` (default: PyCharm)."""
     return {"message": f"Hi, {name}"}
 
